4dc530eb35f0b57593e557a8e35a6af28561dee7
MagicPants <28543678+MagicPantalones@users.noreply.github.com>
2018-02-16 01:09:26 +0100

    #2 Added Activity design

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,18 +2,31 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    TextView originTv;
+    TextView originTitleTv;
+    TextView akaTv;
+    TextView akaTitleTv;
+    TextView ingredientsTv;
+    TextView descriptionTv;
+    TextView descriptionTitleTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,13 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        originTv = findViewById(R.id.origin_tv);
+        akaTv = findViewById(R.id.also_known_tv);
+        akaTitleTv = findViewById(R.id.aka_title_tv);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
+        descriptionTv = findViewById(R.id.description_tv);
+        originTitleTv = findViewById(R.id.origin_title_tv);
+        descriptionTitleTv = findViewById(R.id.description_title_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +63,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +76,37 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        List<String> ingredientsArray = sandwich.getIngredients();
+        List<String> akaArray = sandwich.getAlsoKnownAs();
+        String origin = sandwich.getPlaceOfOrigin();
+        String description = sandwich.getDescription();
+        int ingredientLineBreaks = 0;
+        int akaLinebreaks = 0;
+        for (String ingredient : ingredientsArray){
+            ingredientsTv.append("-" + ingredient + "\n");
+            ingredientLineBreaks += 1;
+        }
+        if (akaArray.isEmpty()){
+            akaTitleTv.setVisibility(View.INVISIBLE);
+            akaTv.setVisibility(View.INVISIBLE);
+        } else {
+            for (String aka : akaArray) {
+                akaTv.append(aka + "\n");
+                akaLinebreaks += 1;
+            }
+        }
+        if (TextUtils.isEmpty(origin)){
+            originTitleTv.setVisibility(View.INVISIBLE);
+            originTv.setVisibility(View.INVISIBLE);
+        }else {
+            originTv.setText(origin);
+            akaLinebreaks += 1;
+        }
+        descriptionTv.setText(description);
+        if (akaLinebreaks > ingredientLineBreaks){
+            ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) descriptionTitleTv.getLayoutParams();
+            params.topToBottom = R.id.also_known_tv;
+        }
     }
 }
